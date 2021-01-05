a951e9b76ceb722d1d32a8524e3529f64c718d63
leonardo.a.ribeiro.21@gmail.com <leonardo.a.ribeiro.21@gmail.com>
2021-01-05 02:36:38 -0300

    PUT / orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.lang.annotation.Retention;
 import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,6 +41,14 @@ public class OrderService {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
